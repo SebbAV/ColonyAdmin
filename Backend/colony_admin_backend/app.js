e215fe228ce7848141555920a3acc3251c5454af
Unknown <jose_mvp55@hotmail.com>
2018-11-16 22:55:37 -0600

    users

--- Backend/colony_admin_backend/app.js
+++ Backend/colony_admin_backend/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/v1', indexRouter);
-app.use('/v1/users', usersRouter);
+app.use('/v1/user', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
