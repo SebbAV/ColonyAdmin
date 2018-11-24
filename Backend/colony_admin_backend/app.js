82621351e2d3df567cb3ee7c530a7861c578f9eb
Unknown <jose@cbqasolutions.com>
2018-11-24 16:42:40 -0600

    Address route done

--- Backend/colony_admin_backend/app.js
+++ Backend/colony_admin_backend/app.js
@@ -8,6 +8,7 @@ var cors = require('cors');
 var indexRouter = require('./routes/api/v1/index');
 var usersRouter = require('./routes/api/v1/user');
 var visitRouter = require('./routes/api/v1/visit');
+var addressRouter = require('./routes/api/v1/address');
 
 var app = express();
 app.use(cors());
@@ -26,14 +27,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/v1', indexRouter);
 app.use('/v1/user', usersRouter);
 app.use('/v1/visit', visitRouter);
+app.use('/v1/address', addressRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
