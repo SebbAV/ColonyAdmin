662a69629927c3c86ed9147da92e9c37c0961fed
Unknown <jose@cbqasolutions.com>
2018-11-25 16:14:08 -0600

    Socket.io Init

---
+++ Backend/colony_admin_backend/bin/server.js
@@ -0,0 +1,8 @@
+var io = require('socket.io')();
+io.sockets.on('connection', (socket) => {
+    console.log("nuevo Cliente conectado con id: " + socket.id);
+    socket.on('sos',(data)=>
+    {
+        io.sockets.emit('sos_request', data);
+    });
+});
