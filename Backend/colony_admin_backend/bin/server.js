2dcfb2cd769e63acbae769094f35b92592e07798
Unknown <jose@cbqasolutions.com>
2018-11-30 23:17:32 -0600

    Socket io client implemented

--- Backend/colony_admin_backend/bin/server.js
+++ Backend/colony_admin_backend/bin/server.js
@@ -1,9 +1,11 @@
 var io = require('socket.io')();
 io.sockets.on('connection', (socket) => {
     console.log("nuevo Cliente conectado con id: " + socket.id);
-    socket.on('sos',(data)=>
-    {
+    socket.on('sos', (data) => {
         io.sockets.emit('sos_request', data);
+    });
+    socket.on('allow_entrance', (data) => {
+        io.sockets.emit('allow_entrance_check', data);
     });
 });
 module.exports = io;
